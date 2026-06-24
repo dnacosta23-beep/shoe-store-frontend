2c5c0310a62c9fa497393362752fd0ec0a1b84e6
Danielle Acosta <dnacosta23@gmail.com>
2026-06-24 18:21:16 -0500

    Setup useState and useEffect.

--- shoe-store-frontend/src/App.jsx
+++ shoe-store-frontend/src/App.jsx
@@ -1,10 +1,27 @@
-
+import { useEffect, useState } from 'react'
 import './App.css'
+
+function App() {
+  const [shoes, setShoes] = useState([])
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+  const [message, setMessage] = useState('')
+
+  async function fetchShoes() {
+    const response = await fetch('http://127.0.0.1:5000/shoes')
+    const data = await response.json()
+    setShoes(data)
+  }
+
+  useEffect(() => {
+    fetchShoes()
+  }, [])
 
   return (
     <>
-      </>
+     
+    </>
   )
-
+}
 
 export default App
